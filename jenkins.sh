2b8001583d7aace6be4a330d9a3f6209c1d68336
munna <satyapadamati5@gmail.com>
2024-08-06 11:55:37 +0530

    simple

---
+++ jenkins.sh
@@ -0,0 +1,7 @@
+sudo yum update -y
+sudo yum install -y java-11-amazon-corretto
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum install -y jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
